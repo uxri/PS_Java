cb197ddea3efe424a9c0a8be9d737a8a097fa0e6
Yuree Choi <102457223+uxri@users.noreply.github.com>
2022-06-08 00:26:50 +0900

    #10171

---
+++ cat.java
@@ -0,0 +1,10 @@
+package first;
+
+public class cat {
+	public static void main(String[] args){
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
