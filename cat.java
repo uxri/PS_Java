dd06dac535692fa953212b23d74f5a02c8a8c381
Yuree Choi <102457223+uxri@users.noreply.github.com>
2022-06-08 00:28:37 +0900

    #10701

--- cat.java
+++ cat.java
@@ -1,6 +1,4 @@
-package first;
-
-public class cat {
+public class Main {
 	public static void main(String[] args){
         System.out.println("\\    /\\");
         System.out.println(" )  ( ')");
