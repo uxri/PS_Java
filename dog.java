f9816853cb2ea11a42434a2d72e2716509337263
Yuree Choi <102457223+uxri@users.noreply.github.com>
2022-06-08 00:27:11 +0900

    #10172

---
+++ dog.java
@@ -0,0 +1,11 @@
+package first;
+
+public class dog {
+	public static void main(String[] args){
+        System.out.println("|\\_/| ");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");    
+        System.out.println("||_/=\\\\__|"); 
+    }
+}
