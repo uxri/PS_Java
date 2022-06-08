9ab133d8686c49926e5799c09b0c757ad605fbd1
Yuree Choi <102457223+uxri@users.noreply.github.com>
2022-06-09 02:00:25 +0900

    #1008

---
+++ divide.java
@@ -0,0 +1,14 @@
+package first;
+
+import java.util.Scanner;
+
+public class divide {
+	 public static void main(String[] args){
+	        
+	        Scanner sc = new Scanner(System.in);
+	        double a,b;
+	        a = sc.nextDouble();
+	        b = sc.nextDouble();
+	        System.out.println(a/b);
+	    }
+}
