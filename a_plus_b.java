182f25187d0b9ede99fc67c5dbe882a733fa610e
Yuree Choi <102457223+uxri@users.noreply.github.com>
2022-06-09 02:00:05 +0900

    #1000

---
+++ a_plus_b.java
@@ -0,0 +1,15 @@
+package first;
+
+
+import java.util.Scanner;
+
+public class a_plus_b {
+	    public static void main(String[] args){
+	        
+	        Scanner sc = new Scanner(System.in);
+	        int a,b;
+	        a = sc.nextInt();
+	        b = sc.nextInt();
+	        System.out.println(a+b);
+	    }
+	}
