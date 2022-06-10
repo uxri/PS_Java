fe858e3732927ce78824529157681c92a7912178
Yuree Choi <102457223+uxri@users.noreply.github.com>
2022-06-11 00:24:52 +0900

    #18108

---
+++ Thailand_age.java
@@ -0,0 +1,9 @@
+package first;
+import java.util.Scanner;
+
+public class Thailand_age {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.nextInt()-543);
+	}
+}
