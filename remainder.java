95eeda1355c9aa6f653a57063519ff5c9e764dc8
Yuree Choi <102457223+uxri@users.noreply.github.com>
2022-06-12 18:01:09 +0900

    #10430

---
+++ remainder.java
@@ -0,0 +1,19 @@
+package first;
+
+import java.util.Scanner;
+
+public class remainder {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int a, b, c;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		c = sc.nextInt();
+
+		System.out.println((a + b) % c);
+		System.out.println(((a % c) + (b % c)) % c);
+		System.out.println((a * b) % c);
+		System.out.println(((a % c) * (b % c)) % c);
+	}
+}
