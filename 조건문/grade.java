5726e59aa64779fe20d56aec18dc11d1ff64369e
Yuree Choi <102457223+uxri@users.noreply.github.com>
2022-06-14 00:28:37 +0900

    #9498

--- 조건문/grade.java
+++ 조건문/grade.java
@@ -3,14 +3,20 @@ package second;
 import java.util.Scanner;
 
 public class grade {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int a;
-		a = sc.nextInt();
+		public static void main(String[] args) {
+			Scanner sc = new Scanner(System.in);
+			int a;
+			a = sc.nextInt();
 
-		if (a%4 == 0 && a%100!= 0 || a%400 == 0)
-			System.out.println("1");
-		else
-			System.out.println("0");
+			if (a >=90)
+				System.out.println("A");
+			else if (a >=80)
+				System.out.println("B");
+			else if (a >= 70)
+				System.out.println("C");
+			else if (a >= 60)
+				System.out.println("D");
+			else
+				System.out.println("F");
 	}
 }
