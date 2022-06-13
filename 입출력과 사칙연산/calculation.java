e6e1f886993112b9e0dc911079e6c756937ef464
Yuree Choi <102457223+uxri@users.noreply.github.com>
2022-06-13 14:17:18 +0900

    #10869

--- 입출력과 사칙연산/calculation.java
+++ 입출력과 사칙연산/calculation.java
@@ -1,8 +1,6 @@
-package first;
-
 import java.util.Scanner;
 
-public class calculation {
+public class Main {
 	public static void main(String[] args){
         
         Scanner sc = new Scanner(System.in);
