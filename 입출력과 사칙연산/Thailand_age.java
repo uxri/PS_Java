8a1334c3352723aafcf3b73e70343dbfbd9ffb53
Yuree Choi <102457223+uxri@users.noreply.github.com>
2022-06-13 14:18:17 +0900

    #18108

--- 입출력과 사칙연산/Thailand_age.java
+++ 입출력과 사칙연산/Thailand_age.java
@@ -1,7 +1,6 @@
-package first;
 import java.util.Scanner;
 
-public class Thailand_age {
+public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println(sc.nextInt()-543);
