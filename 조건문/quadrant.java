11885cdfbe6faf0872058ba7b5b54920db36ce7c
Yuree Choi <102457223+uxri@users.noreply.github.com>
2022-06-15 01:12:31 +0900

    #14681

---
+++ 조건문/quadrant.java
@@ -0,0 +1,21 @@
+package second;
+
+import java.util.Scanner;
+
+public class quadrant {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a, b;
+		a = sc.nextInt();
+		b = sc.nextInt();
+
+		if (a>0 && b>0)
+			System.out.println("1");
+		else if (a<0 && b>0)
+			System.out.println("2");
+		else if (a<0 && b<0)
+			System.out.println("3");
+		else
+			System.out.println("4");
+	}
+}
