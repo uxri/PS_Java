7fa2e3e2d7054a942438816bad1459f0d190feb6
Yuree Choi <yuree0217@gmail.com>
2022-06-15 14:52:50 +0900

    #15552

---
+++ buffer_sum.java
@@ -0,0 +1,24 @@
+package third;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class buffer_sum {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer st;  // StringTokenizer인자값에 입력 문자열 넣음
+		int k = Integer.parseInt(br.readLine()); //int 선언
+		int i;
+		for (i=0; i<k; i++) {
+			st = new StringTokenizer(br.readLine());
+			bw.write((Integer.parseInt(st.nextToken())+Integer.parseInt(st.nextToken()))+"\n");
+		}
+		bw.close();
+	}
+}
